a67c571dd27af8cde3c0aa21d41af1824b6c0505
Benjamin Ottensten <benjamin@ungtriumf.dk>
2014-11-21 15:42:15 +0100

    Ændringer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-//$(document).ready(function(){
-    
-    function toggleSettings() {
-        $(".dropdown").toggle();
-    }
+function toggleSettings() {
+    $(".dropdown_settings").toggle();
+    $(".nav-item-active_settings").toggle();
+}
 
-//});
+function toggleUser() {
+    $(".dropdown_user").toggle();
+    $(".nav-item-active_user").toggle();
+}
+
+function toggleSearch() {
+    $(".dropdown_search").toggle();
+    $(".nav-item-active_search").toggle();
+}
