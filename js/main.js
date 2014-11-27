bc51ac6e8a5b14764fc90d7c987cc49e0a6988f8
Benjamin Ottensten <benjamin@ungtriumf.dk>
2014-11-27 17:34:56 +0100

    Optimized JS and new active classes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-function untoggleDropdown() {
-    $(".dropdown-active .nav-arrow").toggle();
-} 
-
 function toggleSettings() {
-    $(".dropdown_search").css('display', 'none');
-    $(".nav-arrow_search").css('display', 'none');
+    // Remove active class from other nav items
+    $("a").removeClass("nav-item-active");
     
-    $(".dropdown_settings").toggle().addClass('dropdown-active');
+    // Untoggle other dropdowns
+    $(".dropdown_search").hide();
+    $(".nav-arrow_search").hide();
+
+    // Add active class to nav item
+    $(".nav-item_settings").addClass("nav-item-active");
+    
+    // Toggle dropdown and arrow
+    $(".dropdown_settings").toggle();
     $(".nav-arrow_settings").toggle();
 }
 
-function toggleUser() {    
-    $(".dropdown_user").toggle();
-    $(".nav-arrow_user").toggle();
-}
-
 function toggleSearch() {
-    $(".dropdown_settings").css('display', 'none');
-    $(".nav-arrow_settings").css('display', 'none');
+    // Remove active class from other nav items
+    $("a").removeClass("nav-item-active");
     
+    // Untoggle other dropdowns
+    $(".dropdown_settings").hide();
+    $(".nav-arrow_settings").hide();
+    
+    // Add active class to a element
+    $(".nav-item_search").addClass("nav-item-active");
+    
+    // Toggle dropdown and arrow
     $(".dropdown_search").toggle();
     $(".nav-arrow_search").toggle();
+}
+
+function toggleUser() {    
+    // Toggle dropdown and arrow
+    $(".dropdown_user").toggle();
+    $(".nav-arrow_user").toggle();
 }
