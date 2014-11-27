0a7a928529197fcbde4d6a58f48b6944c9abe5b4
simonlind91 <simon_lind91@hotmail.com>
2014-11-27 17:06:09 +0100

    unToggle på andre dropdowns end den aktive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
+function untoggleDropdown() {
+    $(".dropdown-active .nav-item-active").toggle();
+} 
+
 function toggleSettings() {
-    $(".dropdown_settings").toggle();
+    $(".dropdown_search").css('display', 'none');
+    $(".nav-item-active_search").css('display', 'none');
+    
+    $(".dropdown_settings").toggle().addClass('dropdown-active');
     $(".nav-item-active_settings").toggle();
 }
 
-function toggleUser() {
+function toggleUser() {    
     $(".dropdown_user").toggle();
     $(".nav-item-active_user").toggle();
 }
 
 function toggleSearch() {
+    $(".dropdown_settings").css('display', 'none');
+    $(".nav-item-active_settings").css('display', 'none');
+    
     $(".dropdown_search").toggle();
     $(".nav-item-active_search").toggle();
 }
