47d00e0b3b1ff6299266a8147cba9cf28b43f973
Benjamin Ottensten <benjamin@ungtriumf.dk>
2014-11-27 17:14:04 +0100

    New class nav-arrow instead of nav-item-active

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 function untoggleDropdown() {
-    $(".dropdown-active .nav-item-active").toggle();
+    $(".dropdown-active .nav-arrow").toggle();
 } 
 
 function toggleSettings() {
     $(".dropdown_search").css('display', 'none');
-    $(".nav-item-active_search").css('display', 'none');
+    $(".nav-arrow_search").css('display', 'none');
     
     $(".dropdown_settings").toggle().addClass('dropdown-active');
-    $(".nav-item-active_settings").toggle();
+    $(".nav-arrow_settings").toggle();
 }
 
 function toggleUser() {    
     $(".dropdown_user").toggle();
-    $(".nav-item-active_user").toggle();
+    $(".nav-arrow_user").toggle();
 }
 
 function toggleSearch() {
     $(".dropdown_settings").css('display', 'none');
-    $(".nav-item-active_settings").css('display', 'none');
+    $(".nav-arrow_settings").css('display', 'none');
     
     $(".dropdown_search").toggle();
-    $(".nav-item-active_search").toggle();
+    $(".nav-arrow_search").toggle();
 }
