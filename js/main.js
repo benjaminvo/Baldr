e88d11d05691fdab88d2b2d90cacd321ad287a2e
Benjamin Ottensten <benjamin@ungtriumf.dk>
2014-11-27 18:08:52 +0100

    All new javascript

--- js/main.js
+++ js/main.js
@@ -1,37 +1,31 @@
-function toggleSettings() {
-    // Remove active class from other nav items
-    $("a").removeClass("nav-item-active");
+$('.nav-item_settings').click(function() {
     
-    // Untoggle other dropdowns
+    // Remove active class from other nav items
+    $('a.nav-item-active').not(this).removeClass('nav-item-active');
+    // Toggle active class
+    $(this).toggleClass('nav-item-active');
+    
+    // Untoggle search dropdown
     $(".dropdown_search").hide();
     $(".nav-arrow_search").hide();
-
-    // Add active class to nav item
-    $(".nav-item_settings").addClass("nav-item-active");
     
     // Toggle dropdown and arrow
     $(".dropdown_settings").toggle();
     $(".nav-arrow_settings").toggle();
-}
+});
 
-function toggleSearch() {
+$('.nav-item_search').click(function() {
+    
     // Remove active class from other nav items
-    $("a").removeClass("nav-item-active");
-    
-    // Untoggle other dropdowns
+    $('a.nav-item-active').not(this).removeClass('nav-item-active');
+    // Toggle active class
+    $(this).toggleClass('nav-item-active');
+ 
+    // Untoggle settings dropdown
     $(".dropdown_settings").hide();
-    $(".nav-arrow_settings").hide();
-    
-    // Add active class to a element
-    $(".nav-item_search").addClass("nav-item-active");
-    
+    $(".nav-arrow_settings").hide();    
+
     // Toggle dropdown and arrow
     $(".dropdown_search").toggle();
     $(".nav-arrow_search").toggle();
-}
-
-function toggleUser() {    
-    // Toggle dropdown and arrow
-    $(".dropdown_user").toggle();
-    $(".nav-arrow_user").toggle();
-}
+});
